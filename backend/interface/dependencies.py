516438df33999a19bdd476010d7f68a68986f669
Alejandro Moreno Palmero <amopa@outlook.es>
2026-02-13 12:25:04 +0100

    fix: ordenación de imports

--- backend/interface/dependencies.py
+++ backend/interface/dependencies.py
@@ -3,12 +3,12 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ..application.uses_cases.process_incoming_message import ProcessIncomingMessageUseCase
+from ..dependencies import get_db_session
 from ..domain.repositories.message_repository import MessageRepository
 from ..domain.repositories.reservation_repository import ReservationRepository
 from ..infrastructure.persistence.sql_message_repository import SQLMessageRepository
 from ..infrastructure.persistence.sql_reservation_repository import SQLReservationRepository
-from ..application.uses_cases.process_incoming_message import ProcessIncomingMessageUseCase
-from ..dependencies import get_db_session
 
 
 # Repositories
