dd36336922e2c100acf8a07d91ca90990b3a5974
José Andrés Lorenzo Robles <joseandres.l.r_15@hotmail.com>
2026-03-05 13:56:24 +0100

    fix: Redefinir caso de uso "ProcessIncomingMessage" para quitar dependencia de la cola. De esta manera, recibe un mensaje y lo procesa.

--- backend/application/use_cases/process_incoming_message.py
+++ backend/application/use_cases/process_incoming_message.py
@@ -2,44 +2,35 @@
 Recupera el mensaje más antiguo en la cola de mensajes entrantes y lo procesa.
 """
 
-from asyncio import Queue
 from datetime import datetime
 from uuid import uuid4
 
+from ...application.dtos.whatsapp_incoming_message import WhatsappIncomingMessage
 from ...domain.entities.message import Message, Role
 from ...domain.repositories.message_repository import MessageRepository
 
 
 class ProcessIncomingMessageUseCase:
-    def __init__(self, message_repository: MessageRepository, message_queue: Queue[Message]):
+    def __init__(self, message_repository: MessageRepository):
         self.message_repository = message_repository
-        self.message_queue = message_queue
 
-    async def execute(self) -> Message | None:
-        try:
-            # Recuperar el mensaje más antiguo en la cola
-            message = await self.message_queue.get()
-        except Exception:
-            # Manejar el caso en que no hay mensajes en la cola
+    async def execute(self, message: WhatsappIncomingMessage) -> Message | None:
+        # 1. Construir mensaje
+        message_processed = Message(
+            id=str(uuid4()),
+            user_id=message.from_phone,
+            provider_message_id=message.message_id,
+            timestamp=datetime.fromtimestamp(int(message.timestamp)),
+            role=Role.USER,
+            content=message.content,
+        )
+        print(message_processed)
+        if not message_processed.is_valid:
             return None
 
-        # Procesar el mensaje recuperado
+        # 2. Guardar en BBDD
+        await self.message_repository.save(message_processed)
 
-        # 1. Construir mensaje
-        if message:
-            message_processed = Message(
-                id=str(uuid4()),
-                user_id=message.user_id,
-                timestamp=datetime.now(),
-                role=Role.USER,
-                content=message.content,
-            )
-            if not message_processed.is_valid:
-                return None
+        print("saved")
 
-            # 2. Guardar en BBDD
-            await self.message_repository.save(message_processed)
-
-            return message_processed
-
-        return None
+        return message_processed
--- backend/tests/unit/application/uses_cases/test_process_incoming_message.py
+++ backend/tests/unit/application/uses_cases/test_process_incoming_message.py
@@ -1,12 +1,10 @@
-from asyncio import Queue
 from datetime import datetime
 from unittest.mock import AsyncMock
-from uuid import uuid4
 
 import pytest
 
+from backend.application.dtos.whatsapp_incoming_message import WhatsappIncomingMessage
 from backend.application.use_cases.process_incoming_message import ProcessIncomingMessageUseCase
-from backend.domain.entities.message import Message, Role
 from backend.domain.repositories.message_repository import MessageRepository
 
 pytestmark = [
@@ -15,75 +13,48 @@ pytestmark = [
 ]
 
 
-async def test_execute_no_message():
-    # Mock dependencies
-    message_repository = AsyncMock(spec=MessageRepository)
-    message_queue = AsyncMock(spec=Queue)
-
-    # Crear instancia del caso de uso
-    use_case = ProcessIncomingMessageUseCase(message_repository, message_queue)
-
-    # Simular que no hay mensajes en la cola
-    message_queue.get.side_effect = Exception("No messages in queue")
-
-    # Ejecutar el caso de uso
-    result = await use_case.execute()
-
-    # Verificar que el resultado es None
-    assert result is None
-    message_queue.get.assert_called_once()
-
-
 async def test_execute_valid_message():
     # Mock dependencies
     message_repository = AsyncMock(spec=MessageRepository)
-    message_queue = AsyncMock(spec=Queue)
 
     # Crear instancia del caso de uso
-    use_case = ProcessIncomingMessageUseCase(message_repository, message_queue)
+    use_case = ProcessIncomingMessageUseCase(message_repository)
 
     # Simular un mensaje válido en la cola
-    valid_message = Message(
-        id=str(uuid4()),
-        user_id="user123",
-        timestamp=datetime.now(),
-        role=Role.USER,
+    valid_input_message = WhatsappIncomingMessage(
+        message_id="wamid.TEST",
+        from_phone="34600000000",
+        timestamp=str(int(datetime.now().timestamp())),
         content="Hello!",
     )
-    message_queue.get.return_value = valid_message
 
     # Ejecutar el caso de uso
-    result = await use_case.execute()
+    result = await use_case.execute(valid_input_message)
 
     # Verificar que el mensaje fue procesado y guardado
     assert result is not None
     assert result.is_valid is True
     message_repository.save.assert_awaited_once_with(result)
-    message_queue.get.assert_called_once()
 
 
 async def test_execute_invalid_message():
     # Mock dependencies
     message_repository = AsyncMock(spec=MessageRepository)
-    message_queue = AsyncMock(spec=Queue)
 
     # Crear instancia del caso de uso
-    use_case = ProcessIncomingMessageUseCase(message_repository, message_queue)
+    use_case = ProcessIncomingMessageUseCase(message_repository)
 
     # Simular un mensaje inválido en la cola
-    invalid_message = Message(
-        id=str(uuid4()),
-        user_id="user123",
-        timestamp=datetime.now(),
-        role=Role.USER,
-        content="",  # Contenido inválido
+    invalid_input_message = WhatsappIncomingMessage(
+        message_id="wamid.TEST",
+        from_phone="34600000000",
+        timestamp=str(int(datetime.now().timestamp())),
+        content="",
     )
-    message_queue.get.return_value = invalid_message
 
     # Ejecutar el caso de uso
-    result = await use_case.execute()
+    result = await use_case.execute(invalid_input_message)
 
     # Verificar que el mensaje no fue procesado ni guardado
     assert result is None
     message_repository.save.assert_not_awaited()
-    message_queue.get.assert_called_once()
